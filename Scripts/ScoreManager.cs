05cce0d6c024b67be0524e174bfc30395a70802b
Kate Hillyard <katehilly1212@gmail.com>
2024-02-19 14:26:57 -0700

    Update ScoreManager.cs to have winning system

--- Scripts/ScoreManager.cs
+++ Scripts/ScoreManager.cs
@@ -5,7 +5,15 @@ public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager Instance;
 
-private void Awake()
+    public Text scoreTextPlayerOne; // Assign this in the Inspector
+    public Text scoreTextPlayerTwo; // Assign this in the Inspector
+    public Text winningText; // Assign this in the Inspector for displaying the winning message
+
+    private int scorePlayerOne;
+    private int scorePlayerTwo;
+    private int jumpsToWin = 4; // Number of jumps required to win
+
+    private void Awake()
     {
         if (Instance == null)
         {
@@ -17,23 +25,38 @@ private void Awake()
         }
     }
 
-    public Text scoreTextPlayerOne; // Assign this in the Inspector
-    public Text scoreTextPlayerTwo; // Assign this in the Inspector
-
-    private int scorePlayerOne;
-    private int scorePlayerTwo;
-
-
     public void PlayerOneScored()
     {
         scorePlayerOne++;
-        scoreTextPlayerOne.text = "Player One: " + scorePlayerOne; // Update the Text element
+        scoreTextPlayerOne.text = "Player1: " + scorePlayerOne; // Update the Text element
+        CheckForWinner();
         Debug.Log("Player One Scored! New score: " + scorePlayerOne);
     }
 
     public void PlayerTwoScored()
     {
         scorePlayerTwo++;
-        scoreTextPlayerTwo.text = "Player Two: " + scorePlayerTwo; // Update the Text element
+        scoreTextPlayerTwo.text = "Player2: " + scorePlayerTwo; // Update the Text element
+        CheckForWinner();
+        Debug.Log("Player Two Scored! New score: " + scorePlayerTwo);
+    }
+
+    private void CheckForWinner()
+    {
+        if (scorePlayerOne >= jumpsToWin)
+        {
+            DisplayWinningMessage("Player1 Won!");
+        }
+        else if (scorePlayerTwo >= jumpsToWin)
+        {
+            DisplayWinningMessage("Player2 Won!");
+        }
+    }
+
+    private void DisplayWinningMessage(string message)
+    {
+        winningText.text = message;
+        winningText.gameObject.SetActive(true);
     }
 }
+
